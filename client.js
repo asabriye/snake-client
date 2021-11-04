68ad034e27e9559d370aa511cf1abc9bc36339aa
asabriye <abdullahisabriye@gmail.com>
2021-11-04 02:49:33 +0000

    updated client to connect to play.js

--- client.js
+++ client.js
@@ -1,11 +1,29 @@
-const net = require("net");
+// const net = require("net");
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: 50541
+//   })
+//   conn.setEncoding('utf8');
+//   return conn
+// }
+  
+//  module.exports = connect
+
+
+ const net = require("net");
+// establishes a connection with the game server
 const connect = function () {
+  
   const conn = net.createConnection({
     host: '165.227.47.243',
-    port: 50541
-  })
-  conn.setEncoding('utf8');
-  return conn
-}
+    port: 50541,
+  });
   
- module.exports = connect
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+  
+};
+module.exports = connect
