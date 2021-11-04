ce61ab81b04ea7047b1f2159150ae2b466352be1
asabriye <abdullahisabriye@gmail.com>
2021-11-04 12:35:36 +0000

    updated play

--- play.js
+++ play.js
@@ -1,38 +1,8 @@
-// const net = require("net");
-// const connect = require("./client");
-// // establishes a connection with the game server
-// // // const connect = function () {
-// //   const conn = net.createConnection({
-// //     host: '165.227.47.243',
-// //     port: 50541
-// //   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-//   conn.on('data', data => console.log(data));
-
-//   // return conn;
-
-
-// console.log("Connecting ...");
-// connect();
-
-
-// const net = require("net");
-// const { connects } = require('./client');
-// // establishes a connection with the game server
-// const connect = function () {
-  
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//     port: 50541,
-//   });
-  
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-  
-//   return conn;
-// // };
 const connect = require('./client');
-console.log("Connecting ...");
-connect();
+const setupInput = require('./input')
+// const stdin = process.stdin;
+
+
+console.log("Connecting ... ");
+let connection = connect();
+setupInput(connection); 
